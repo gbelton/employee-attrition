bb5510aabe117f42df82cc03765a7bc2195d9e6d
Gerald Belton <gerald.belton@gmail.com>
2019-01-12 11:14:55 -0500

    initial commit

---
+++ attrition.R
@@ -0,0 +1,8 @@
+################################################################################
+# Experimenting with machine learning for employee retention using sample data
+# data from: https://www.kaggle.com/HRAnalyticRepository/employee-attrition-data
+#
+# Initial idea: Use Random Forest Regression to predict employee turnover
+#
+################################################################################
+
